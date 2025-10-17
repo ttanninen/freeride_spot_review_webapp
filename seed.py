e0556ddbb8ebda737585ff9424cfdd441795e572
ttanninen <teemu@TEPU.localdomain>
2025-10-17 22:15:12 +0300

    Optimized speed by doing less commits

--- seed.py
+++ seed.py
@@ -20,21 +20,26 @@ with get_connection() as db:
     db.execute("DELETE FROM messages")
     db.execute("DELETE FROM spots")
     db.execute("DELETE FROM users")
-    db.commit()
 
 
 def add_dummy_users(i):
-    usernames = ["".join(random.choices(string.ascii_letters, k=random.randint(3,8))) for _ in range(i)]
+    con = get_connection()
+    cur =  con.cursor()
     password_hash = "test"
-
-    for username in usernames:
-        sql = "INSERT INTO users (username, password_hash, added_at) VALUES (?, ?, datetime('now'))"
-        execute(sql, [username, password_hash])
+    users = [(f"user_{k}", password_hash) for k in range(1, i + 1)]
+    sql = "INSERT INTO users (username, password_hash, added_at) VALUES (?, ?, datetime('now'))"
+    cur.executemany(sql, users)
+    con.commit()
+    con.close()
     return print("database.db populated with example user data")
 
-def add_dummy_spots(i):
+def add_dummy_spots(i, user_count):
+    con = get_connection()
+    cur = con.cursor()
+    spots = []
+
     for _ in range(i):
-        user_id = random.randint(1, i)
+        user_id = random.randint(1, user_count)
         country_id = 115
         continent_id = 3
         title = ''.join(random.choices(string.ascii_letters, k=random.randint(3, 8)))
@@ -42,27 +47,40 @@ def add_dummy_spots(i):
         skill_level_id = random.randint(1, 5)
         aspect = "N"
         notes = ''.join(random.choices(string.ascii_letters + string.digits + ' ', k=random.randint(5, 1000)))
+        spots.append((user_id, country_id, continent_id, title, max_incline, skill_level_id, aspect, notes))
         sql = ("""INSERT INTO spots (user_id, country_id, continent_id, title, max_incline, skill_level_id, aspect, notes, added_at)
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, datetime('now'))
             """)
-        execute(sql, [user_id, country_id, continent_id, title, max_incline, skill_level_id, aspect, notes])
+        cur.executemany(sql, spots)
+    con.commit()
+    con.close()
     return print("database.db populated with example spot data")
 
 
-def add_dummy_messages(i):
-    for k in range(i):
-        user_id = random.randint(1,i)
-        spot_id = random.randint(1,i)
-        content = ''.join(random.choices(string.ascii_letters + string.digits + ' ', k=random.randint(1, 500)))
-        sql = """INSERT INTO messages (user_id, spot_id, content, sent_at) VALUES (?, ?, ?, datetime('now'))"""
-        execute(sql, [user_id, spot_id, content])
+def add_dummy_messages(i, user_count, spot_count):
+    con = get_connection()
+    cur = con.cursor()
+    messages = []
 
+    for _ in range(i):
+        user_id = random.randint(1, user_count)
+        spot_id = random.randint(1, spot_count)
+        content = ''.join(random.choices(string.ascii_letters + string.digits + ' ', k=random.randint(1, 500)))
+        messages.append((user_id, spot_id, content))
+        sql = """INSERT INTO messages (user_id, spot_id, content, sent_at) VALUES (?, ?, ?, datetime('now'))"""
+        cur.executemany(sql, messages)
+    con.commit()
+    con.close()
     return print("database.db populated with example message data")
 
 
-add_dummy_users(1000)
-add_dummy_spots(500)
-add_dummy_messages(300)
+n_users = 1000
+n_spots = 2000
+n_messages = 400
+
+add_dummy_users(n_users)
+add_dummy_spots(n_spots, n_users)
+add_dummy_messages(n_messages, n_users, n_spots)
 
 db.commit()
 db.close()
